3df0658df0573235267f0b29287e21b4027e4753
qingfeng.zhang <804875728@qq.com>
2021-01-19 22:32:20 +0800

    基础路由搭建

--- .umirc.ts
+++ .umirc.ts
@@ -4,5 +4,13 @@ export default defineConfig({
   nodeModulesTransform: {
     type: 'none',
   },
-  routes: [{ path: '/', component: '@/pages/index' }],
+  routes: [
+    {
+      path: '/',
+      component: '@/pages/index',
+      routes: [
+        { path: '/echarts/demo', component: '@/pages/echarts-demo/index' },
+      ],
+    },
+  ],
 });
